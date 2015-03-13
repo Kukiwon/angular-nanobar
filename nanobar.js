3953c77cbca83924f7014e80b5dc671d82865a94
Jordy van Kuijk <jvankuijk@me.com>
2015-03-13 17:43:30 +0100

    added percentage and color options and fixed bug in updatePercentage

--- nanobar.js
+++ nanobar.js
@@ -1,34 +1,36 @@
-(function() {
-  'use strict';
-  angular.module('nanobar', [])
-    .directive('nanobar', function ($log) {
-      return {
-        restrict: 'A',
-        link: function postLink(scope, element, attrs) {
-          if (!Nanobar) {
-            $log.error('Nanobar unavailable');
-            return;
-          }
-          
-          // @TODO options
-          var options = {
-            target: element
-          };
-          var nanobar = new Nanobar(options);
-          
-          function updatePercentage(percentage) {
-            if (!nanobar || typeof(nanobar.go) !== 'function') {
-              return;
-            }
-            nanobar.go(percentage);
-          }
+'use strict';
+angular.module('nanobar', [])
+.directive('nanobar', function ($log) {
+  return {
+    restrict: 'A',
+    scope: {
+      percentage: '=',
+      color: '='
+    },
+    link: function postLink(scope, element, attrs) {
+      if (!Nanobar || element.length > 1) {
+        $log.error('Nanobar unavailable or too many nanobars found');
+        return;
+      }
 
-          scope.$watch('percentage', function(oldPercentage, newPercentage) {
-            if (newPercentage) {
-              updatePercentage(newPercentage);
-            }
-          });
-        }
+      // @TODO options
+      var options = {
+        target: element[0],
+        bg: scope.color ? scope.color : '#000'
       };
-    });
-})();
+      var nanobar = new Nanobar(options);
+      
+      function updatePercentage(percentage) {
+        if (!nanobar || typeof(nanobar.go) !== 'function') {
+          return;
+        }
+        nanobar.go(percentage);
+      }
+      scope.$watch('percentage', function(newPercentage, oldPercentage) {
+        if (newPercentage) {
+          updatePercentage(newPercentage);
+        }
+      });
+    }
+  };
+});
